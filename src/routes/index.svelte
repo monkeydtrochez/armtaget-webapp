e7beccbe24a729cfa71ac817048077940374c604
Daniel Trochez <daniel@queenslab.se>
2021-08-20 16:37:49 +0200

    Migrated to sveltekit and installed TailwindCss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,67 +1,18 @@
-<script>
-	import successkid from 'images/successkid.jpg';
-</script>
 
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Verdi Products</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<nav class="font-sans bg-white text-center flex justify-between my-4 mx-auto container overflow-hidden">
+	<a href="/" class="block text-left">
+		<img src="https://stitches.hyperyolo.com/images/logo.png" class="h-10 sm:h-10 rounded-full" alt="logo">
+	</a>
+	<ul class="text-sm text-gray-700 list-none p-0 flex items-center">
+		<li><a href="/" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline">Om oss</a></li>
+		<li><a href="/" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline">Produkter</a></li>
+		<li class="pr-2"><a href="/" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline">Kunder</a></li>
+		<li class="pl-2 border-l"><a href="/" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline">Kontakt</a></li>
+	</ul>
+</nav>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading..</strong></p>
-
-<form>
-	<div class="mb-3">
-	  <label for="exampleInputEmail1" class="form-label">Email address</label>
-	  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-	  <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-	</div>
-	<div class="mb-3">
-	  <label for="exampleInputPassword1" class="form-label">Password</label>
-	  <input type="password" class="form-control" id="exampleInputPassword1">
-	</div>
-	<div class="mb-3 form-check">
-	  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-	  <label class="form-check-label" for="exampleCheck1">Check me out</label>
-	</div>
-	<button type="submit" class="btn btn-primary">Submit</button>
-  </form>
