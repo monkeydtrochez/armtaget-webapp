adc3a0b0d813e2ecf906ea1b05fbdc6323c7d79a
Daniel Trochez <daniel@queenslab.se>
2021-08-31 22:53:29 +0200

    finished markup for contact form and added form validation

--- src/components/Forms/ContactForm.svelte
+++ src/components/Forms/ContactForm.svelte
@@ -1,17 +1,28 @@
 <script>
     import {createEventDispatcher} from "svelte";
+    import {notEmpty, isValidEmail} from "../../helpers/formValidation";
     import Modal from "../UI/Modal.svelte";
     import Button from "../UI/Button.svelte";
-
+    import Input from "../UI/Input.svelte";
+    
     let dispatch = createEventDispatcher();
-
+    
     export let leftBtnText;
     export let rightBtnText;
+    
+    let firstName = "";
+    let lastName = "";
+    let emailAddress = "";
+    
+    $: isFirstNameValid = !notEmpty(firstName);
+    $: isLastNameValid = !notEmpty(lastName);
+    $: isEmailValid = !notEmpty(emailAddress) && isValidEmail(emailAddress);
+    $: isFormValid = isFirstNameValid && isLastNameValid && isEmailValid;
     
     function closeModal() {
         dispatch('cancel');
     }
-
+    
     function submit() {
         console.log("Submit form - not yet implemented...");
         dispatch('cancel');
@@ -22,30 +33,41 @@
     <form class="w-full max-w-lg">
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                    Förnamn
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" >
-                <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+                <Input 
+                id="firstname" 
+                label="Förnamn" 
+                value={firstName}
+                isValid={isFirstNameValid} 
+                errorMessage="Du måste ange förnamn" 
+                isMandatoryField={true}
+                on:input={event => (firstName = event.target.value)} /> 
             </div>
             <div class="w-full md:w-1/2 px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                    Efternamn
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" >
+                <Input 
+                id="lastname" 
+                label="Efternamn" 
+                value={lastName}
+                isValid={isLastNameValid} 
+                errorMessage="Du måste ange efternamn" 
+                isMandatoryField={true}
+                on:input={event => (lastName = event.target.value)} />
             </div>
         </div>
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                    Mejladress
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="email" >
+                <Input 
+                id="email" 
+                label="Mejladress" 
+                value={emailAddress}
+                isValid={isEmailValid} 
+                errorMessage="Du måste ange en giltig mejladress" 
+                isMandatoryField={true}
+                on:input={event => (emailAddress = event.target.value)} />
             </div>
         </div>
     </form>
     <div slot="footer" class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-        <Button btnText={rightBtnText} on:click={submit}/>
+        <Button btnText={rightBtnText} disabled={!isFormValid} on:click={submit}/>
         <Button btnText={leftBtnText} on:click={closeModal}/>
     </div>
 </Modal>
--- src/components/UI/Button.svelte
+++ src/components/UI/Button.svelte
@@ -4,11 +4,10 @@
     let dispatch = createEventDispatcher();
 
     export let btnText;
-
-
+    export let disabled = false;
 </script>
 
-<button class=" bg-black hover:bg-gray-900 text-white hover:text-white py-3 px-6 uppercase text-xs tracking-wide border border-white"
+<button disabled={disabled} class="disabled:bg-opacity-50  bg-black hover:bg-gray-900 text-white hover:text-white py-3 px-6 uppercase text-xs tracking-wide border border-white"
 on:click={() => dispatch('click')}>
     {btnText}
 </button>
