c2801ccb3cd08ce84b74ad707645db44d3ec0399
Daniel Trochez <daniel@queenslab.se>
2021-09-03 08:49:17 +0200

    fixed so nav links scrolls to their sections

--- src/components/UI/Navigation.svelte
+++ src/components/UI/Navigation.svelte
@@ -1,12 +1,5 @@
 <script>
-	import {createEventDispatcher} from "svelte";
 	import LogoImage from "../../../static/assets/verdi-logo-white.jpg"
-	
-	const dispatch = createEventDispatcher();
-
-	function navigateTo(section) {
-		dispatch('navigate', section);
-	}
 </script>
 
 <nav class="font-sans bg-white text-center flex justify-between my-4 mx-auto container overflow-hidden">
@@ -14,9 +7,9 @@
 		<img src="{LogoImage}" class="h-10 sm:h-20 " alt="logo">
 	</a>
 	<ul class="text-sm text-gray-700 list-none p-0 flex items-center">
-		<li><a href="/" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" on:click={navigateTo("about-us")}>Om oss</a></li>
-		<li><a href="/" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" on:click={navigateTo("products")}>Produkter</a></li>
-		<li class="pr-2"><a href="/" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" on:click={navigateTo("clients")}>Kunder</a></li>
-		<li class="pl-2 border-l"><a href="/" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" on:click={navigateTo("contact")}>Kontakt</a></li>
+		<li><a href="#about-us" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" >Om oss</a></li>
+		<li><a href="#products" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" >Produkter</a></li>
+		<li class="pr-2"><a href="#clients" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" >Kunder</a></li>
+		<li class="pl-2 border-l"><a href="#contact" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" >Kontakt</a></li>
 	</ul>
 </nav>
