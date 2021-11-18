ac708da6f221046bcd310bc2ef7e1ce709f0ceb0
Daniel Trochez <daniel@queenslab.se>
2021-11-18 18:31:09 +0100

    fixed bug in mobile menu

--- src/components/HeroBanner.svelte
+++ src/components/HeroBanner.svelte
@@ -2,6 +2,7 @@
     import HeroImage from "../../static/assets/verdi-hero-logo.jpg"
 </script>
 
-<div class="w-full h-full bg-no-repeat bg-cover bg-center bg-fixed"
+<!-- TODO Gör om till img element istället kolla på att använda olika bilder vid olika brytpunkter -->
+<div class="h-full bg-no-repeat bg-cover bg-center"
     style="background-image: url('{HeroImage}');">
 </div>
--- src/components/UI/Navigation.svelte
+++ src/components/UI/Navigation.svelte
@@ -1,5 +1,15 @@
 <script>
 	import LogoImage from "../../../static/assets/verdi-nav-logo.jpg"
+	
+	let showMobileMenu = false;
+
+	function toggleMobileMenu() {
+		if (showMobileMenu) {
+			showMobileMenu = false;
+			return;
+		}
+		showMobileMenu = true;
+	}
 </script>
 
 <div class="h-25 bg-white">
@@ -23,7 +33,7 @@
 				
 				<!-- Mobile menu button -->
 				<div class="md:hidden flex items-center">
-					<button class="outline-none mobile-menu-button">
+					<button class="outline-none mobile-menu-button" on:click={toggleMobileMenu}>
 						<svg class=" w-6 h-6 text-gray-500 hover:text-green-500 "
 						x-show="!showMenu"
 						fill="none"
@@ -40,37 +50,15 @@
 		</div>		
 	</div>
 	<!-- mobile menu -->
-	<div class="hidden mobile-menu">
+	<div class="{showMobileMenu ? '' : 'hidden'} mobile-menu">
 		<ul class="">
-			<li class="#about-us"><a href="index.html" class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Om oss</a></li>
+			<li><a href="#about-us" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Om oss</a></li>
 			<li><a href="#products" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Produkter</a></li>
 			<li><a href="#clients" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Kunder</a></li>
 			<li><a href="#contact" class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Kontakta oss</a></li>
 		</ul>
 	</div>
-	<script>
-		const btn = document.querySelector("button.mobile-menu-button");
-		const menu = document.querySelector(".mobile-menu");
-		
-		btn.addEventListener("click", () => {
-			menu.classList.toggle("hidden");
-		});
-	</script>
 </nav>
 </div>
-
-<!-- <div class="h-25 bg-white">
-	<nav class="font-sans w-10/12 text-center flex justify-between my-4 mx-auto overflow-hidden">
-		<a href="/" class="block text-left">
-			<img src="{LogoImage}" class="h-10 sm:h-20 " alt="logo">
-		</a>
-		<ul class="text-sm text-gray-700 list-none p-0 flex items-center">
-			<li><a href="#about-us" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" >Om oss</a></li>
-			<li><a href="#products" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" >Produkter</a></li>
-			<li class="pr-2"><a href="#clients" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" >Kunder</a></li>
-			<li class="pl-2 border-l"><a href="#contact" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline" >Kontakt</a></li>
-		</ul>
-	</nav>
-</div> -->
 
 
