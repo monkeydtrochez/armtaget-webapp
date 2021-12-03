f477a9cc31a7d2c752b9846eb8b01c76878ec560
Daniel Trochez <daniel@queenslab.se>
2021-12-03 14:47:42 +0100

    Responsive text and footer fixes for small screens

--- src/components/Products.svelte
+++ src/components/Products.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div class="w-full flex justify-center mb-3">
-    <div class="w-6/12 text-gray-900 mt-6 md:mt-0 m-0 leading-normal">
+    <div class="sm:w-9/12 md:w-6/12 lg:w-6/12 text-gray-900 mt-6 md:mt-0 m-0 leading-normal">
         <h4 class="pt-2 pb-8 uppercase font-bold tracking-wide">Produkter</h4>
         <div class="my-0 font-sans container max-w-3xl m-auto flex flex-col lg:flex-row text-center lg:text-left sm:items-center">
-            <div class="w-full lg:w-1/2">
+            <div class="lg:w-1/2">
                 <img src="{ProductImg}" alt="arm-tag" class="rounded-3xl">
             </div>
             <div class="w-full lg:w-1/2 flex flex-col justify-center p-8 md-tracking-wide items-center lg:items-start">
--- src/components/UI/Footer.svelte
+++ src/components/UI/Footer.svelte
@@ -4,9 +4,9 @@
 
 </script>
 
-<footer class="font-sans bg-black text-white py-8 px-4 flex items-stretch">
-    <div class="mx-auto max-w-xl overflow-hidden flex justify-between items-center">
-        <ul class="text-sm text-gray-700 list-none p-0 flex items-center">
+<footer class="font-sans bg-black text-white py-8 px-4">
+    <div class="mx-auto max-w-xl flex justify-between">
+        <ul class="text-sm text-gray-700 list-none p-0">
             <li> <a href="/" class="block mr-4">
                 <img src="{LogoImage}" class="h-10 sm:h-20" alt="logo">
             </a></li>
@@ -15,7 +15,7 @@
             <li><a href="#clients" class="inline-block py-2 px-3 text-gray-500 hover:text-gray-400 no-underline" >Kunder</a></li>
             <li><a href="#contact" class="inline-block py-2 px-3 text-gray-500 hover:text-gray-400 no-underline" >Kontakt</a></li>
         </ul>
-        <p class="inline-block py-2 px-3 text-gray-700 text-xs"> ©2021 Verdi Products</p>
+        <Social />
     </div>
-    <Social />
+    <p class="inline-block py-2 px-3 text-gray-700 text-xs"> ©2021 Verdi Products</p>
 </footer>
