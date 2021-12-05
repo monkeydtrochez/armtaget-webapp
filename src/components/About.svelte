b6bba30db039d2d5f04bc3b1d8ee29b22cc8cd49
Daniel Trochez <daniel@queenslab.se>
2021-12-05 19:44:47 +0100

    Added correct about us text and example video

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -2,11 +2,24 @@
 <div class="w-full flex justify-center mb-3">
     <div class="sm:w-4/12/12 md:w-6/12 lg:w-6/12 text-gray-900 mt-6 leading-normal text-center">
         <h4 class="py-8 uppercase font-bold tracking-wide">Om oss</h4>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-            a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-            of Lorem Ipsum.</p>
-    </div>
-</div>
+        <p>Konceptet Armtaget har legat och grott i idéstadiet en längre tid - nu under Covid-19 pandemin blev ideén äntligen till verklighet.
+            De flesta butiksbesökare använder sina händer för att öppna kylar, frysar, plocka frukt och matvaror. Många använder också sina händer för att kolla inköpslistan på telefonen, omedvetet klia sig i ansiktet eller assistera sitt barn med snuvan. Ofta gör människor detta helt omedvetet vilket gör att bakterier och visrus sprids. Med hjälp av Armtaget kan vi tillsammans minska smittspridning utan allt för stora åtgärder i vardagen.</p>
+            <p>Armtaget är ett steg i rätt riktning för en bättre hygienstandard i butiker. Syftet är helt enkelt att det ska vara smidigt för kunder att förstå och använda. Tillsammans med butikschefer och handlare samarbetar vi mot ett friskare samhälle som alla vinner på - öppet, ärligt och hälsosamt!</p>
+            
+            <!-- <iframe class="w-full rounded-3xl mt-4"
+                title="campaign video"
+                src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FVerdiProducts%2Fvideos%2F257112312962079%2F&show_text=false&width=476&t=0"
+                style="border:none;"
+                height="476"
+                scrolling="no"
+                frameborder="0"
+                allowfullscreen="true"
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                allowFullScreen="true">
+            </iframe> -->
+            <video class="w-full rounded-3xl mt-4" autoplay loop muted>
+                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" type="video/mp4" />
+                <track kind="captions">
+                </video>
+            </div>
+        </div>
