054c6f3f27b4da2b513de09dc7031d561f307c81
Daniel Trochez <daniel@queenslab.se>
2021-12-05 21:48:09 +0100

    Updates to product view

--- src/components/Products.svelte
+++ src/components/Products.svelte
@@ -1,18 +1,17 @@
 <script>
-    import ProductImg from "../../static/assets/product.jpg";
     import ProductImg1 from "../../static/assets//products/produkt-bild-1.jpg";
     import ProductImg2 from "../../static/assets/products/produkt-bild-2.jpg";
 </script>
 
 <div class="w-full flex justify-center mb-3">
-    <div class="sm:w-9/12 md:w-6/12 lg:w-6/12 text-gray-900 mt-6 md:mt-0 m-0 leading-normal">
+    <div class="text-gray-900 mt-6 md:mt-0 m-0 sm:w-4/12/12 md:w-6/12 lg:w-6/12 leading-normal text-center">
         <h4 class="pt-2 pb-8 uppercase font-bold tracking-wide">Produkter</h4>
-        <div class="my-0 font-sans container max-w-3xl m-auto flex flex-col lg:flex-row text-center lg:text-left sm:items-center">
-            <div class="w-1/2 flex justify-center">
+        <div class="my-0 font-sans">
+            <div class="w-1/2 flex justify-between">
                 <img src="{ProductImg1}" alt="arm-tag" class="rounded-3xl">
                 <img src="{ProductImg2}" alt="arm-tag" class=" ml-4 rounded-3xl">
             </div>
-            <div class="w-full lg:w-1/2 flex flex-col justify-center p-8 md-tracking-wide items-center lg:items-start">
+            <div class="mt-4">
                 <label for="tagline" class="uppercase tracking-wide text-gray-700 font-bold">Dags för nya tag - Armtag!</label>
                 <h1 class="my-4 font-normal">Armtaget, med sin enkla och smidiga konstruktion gör den lätt att installera själv eller med hjälp från oss.
                 </h1>
