d098d59afe3ace7c5ca73258e1fba50369dc82a9
Daniel Trochez <daniel@queenslab.se>
2021-12-05 19:55:05 +0100

    added video and moved video player

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,6 +1,12 @@
 
 <div class="w-full flex justify-center mb-3">
     <div class="sm:w-4/12/12 md:w-6/12 lg:w-6/12 text-gray-900 mt-6 leading-normal text-center">
+
+        <video class="w-full rounded-3xl mt-4" autoplay loop muted>
+            <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" type="video/mp4" />
+            <track kind="captions">
+            </video>
+            
         <h4 class="py-8 uppercase font-bold tracking-wide">Om oss</h4>
         <p>Konceptet Armtaget har legat och grott i idéstadiet en längre tid - nu under Covid-19 pandemin blev ideén äntligen till verklighet.
             De flesta butiksbesökare använder sina händer för att öppna kylar, frysar, plocka frukt och matvaror. Många använder också sina händer för att kolla inköpslistan på telefonen, omedvetet klia sig i ansiktet eller assistera sitt barn med snuvan. Ofta gör människor detta helt omedvetet vilket gör att bakterier och visrus sprids. Med hjälp av Armtaget kan vi tillsammans minska smittspridning utan allt för stora åtgärder i vardagen.</p>
@@ -17,9 +23,5 @@
                 allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
                 allowFullScreen="true">
             </iframe> -->
-            <video class="w-full rounded-3xl mt-4" autoplay loop muted>
-                <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" type="video/mp4" />
-                <track kind="captions">
-                </video>
             </div>
         </div>
