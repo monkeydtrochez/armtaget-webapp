5a7cf1276f4fbda8dd8b847731a8686aed593364
Daniel Trochez <daniel@queenslab.se>
2021-12-05 20:43:00 +0100

    added new product images and mobile video

--- src/components/Products.svelte
+++ src/components/Products.svelte
@@ -1,23 +1,25 @@
 <script>
     import ProductImg from "../../static/assets/product.jpg";
+    import ProductImg1 from "../../static/assets//products/produkt-bild-1.jpg";
+    import ProductImg2 from "../../static/assets/products/produkt-bild-2.jpg";
 </script>
 
 <div class="w-full flex justify-center mb-3">
     <div class="sm:w-9/12 md:w-6/12 lg:w-6/12 text-gray-900 mt-6 md:mt-0 m-0 leading-normal">
         <h4 class="pt-2 pb-8 uppercase font-bold tracking-wide">Produkter</h4>
         <div class="my-0 font-sans container max-w-3xl m-auto flex flex-col lg:flex-row text-center lg:text-left sm:items-center">
-            <div class="lg:w-1/2">
-                <img src="{ProductImg}" alt="arm-tag" class="rounded-3xl">
+            <div class="w-1/2 flex justify-center">
+                <img src="{ProductImg1}" alt="arm-tag" class="rounded-3xl">
+                <img src="{ProductImg2}" alt="arm-tag" class=" ml-4 rounded-3xl">
             </div>
             <div class="w-full lg:w-1/2 flex flex-col justify-center p-8 md-tracking-wide items-center lg:items-start">
                 <label for="tagline" class="uppercase tracking-wide text-gray-700 font-bold">Dags för nya tag - Armtag!</label>
-                <h1 class="my-4 font-normal">Armtaget, med sin enkla och smidiga konstruktion gör den enkel att installera själv eller med hjälp från oss.
-                    Den har även visat sig vara mycket uppskattad hos handlare och deras kunder, vilket vi såklart är väldigt stolta över.
+                <h1 class="my-4 font-normal">Armtaget, med sin enkla och smidiga konstruktion gör den lätt att installera själv eller med hjälp från oss.
                 </h1>
-                <p class="leading-normal mb-4 text-gray-700">Armtagen är gjorda i Pteg, samma material som idag finns i PET-flaskor.
-                    Med andra ord så är våra armtag 100% förnybara och riktigt tåliga. Armtagen fästes lätthet med hjälp av våra 
-                    rostfria metallvinklar, som måttanpassas för att passa just era unika krav.
-                </p>
+                <p class="leading-normal mb-4 text-gray-700">Ingen nuvarande inredning behöver tas bort. Ni behöver varken borra eller limma och det kräver inga stora ingrepp för installation.
+                    Armtagen kommer färdigmonterade med akrylgummin. Fördelen med detta är att inget jobbigt klister fastnar på ytan om något behövs korrigeras eller bytas ut.</p>
+                <p class="leading-normal mb-4 text-gray-700">Armtagen är gjorda i Pteg, samma material som finns idag i PET-flaskor. Med andra ord så är de 100% förnybara och riktigt tåliga.
+                    Vi kan även anpassa Armtag efter önskat mått och design.</p>
                 <p class="leading-normal mb-4 text-gray-700">
                     För mer info kontakta oss via formuläret nedan!
                 </p>
