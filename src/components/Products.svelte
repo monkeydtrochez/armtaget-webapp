9dbe299b3f23e97fd51f6faab94d21e58e5fcb3f
Daniel Trochez <daniel@queenslab.se>
2021-12-08 22:56:44 +0100

    added third product image

--- src/components/Products.svelte
+++ src/components/Products.svelte
@@ -1,6 +1,7 @@
 <script>
     import ProductImg1 from "../../static/assets//products/produkt-bild-1.jpg";
     import ProductImg2 from "../../static/assets/products/produkt-bild-2.jpg";
+    import ProductImg3 from "../../static/assets/products/produkt-bild-3.jpg";
 </script>
 
 <div class="w-full flex justify-center mb-3">
@@ -9,7 +10,7 @@
         <div class="my-0 font-sans">
             <div class="w-1/2 flex justify-between">
                 <img src="{ProductImg1}" alt="arm-tag" class="rounded-3xl">
-                <img src="{ProductImg2}" alt="arm-tag" class=" ml-4 rounded-3xl">
+                <img src="{ProductImg2}" alt="arm-tag" class="mx4 rounded-3xl">
             </div>
             <div class="mt-4">
                 <label for="tagline" class="uppercase tracking-wide text-gray-700 font-bold">Dags för nya tag - Armtag!</label>
@@ -19,7 +20,10 @@
                     Armtagen kommer färdigmonterade med akrylgummin. Fördelen med detta är att inget jobbigt klister fastnar på ytan om något behövs korrigeras eller bytas ut.</p>
                 <p class="leading-normal mb-4 text-gray-700">Armtagen är gjorda i Pteg, samma material som finns idag i PET-flaskor. Med andra ord så är de 100% förnybara och riktigt tåliga.
                     Vi kan även anpassa Armtag efter önskat mått och design.</p>
-                <p class="leading-normal mb-4 text-gray-700">
+                    <div class="w-full flex justify-center" style="height: 480px;">
+                        <img src="{ProductImg3}" alt="arm-tag" class="rounded-3xl">
+                    </div>
+                <p class="leading-normal mt-4 mb-4 font-bold text-gray-700">
                     För mer info kontakta oss via formuläret nedan!
                 </p>
             </div>
